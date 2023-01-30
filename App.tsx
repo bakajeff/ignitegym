a17f8c7ed443db06726b31e9b243e214ec34716a
jeffte costa <jefftewf17@outlook.com>
2023-01-30 16:46:22 -0300

    feat: add SignUp screen

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@ import { NativeBaseProvider } from "native-base";
 
 import { THEME } from "./src/theme";
 import { Loading } from "@components/Loading";
-import { SignIn } from "@screens/SignIn";
+import { SignUp } from "@screens/SignUp";
 
 export default function App() {
 	const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -20,7 +20,7 @@ export default function App() {
 				backgroundColor="transparent"
 				translucent
 			/>
-			{fontsLoaded ? <SignIn /> : <Loading />}
+			{fontsLoaded ? <SignUp /> : <Loading />}
 		</NativeBaseProvider>
 	);
 }
