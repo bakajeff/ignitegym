5159d6754c6e381c76451a26e2b56edaf97c52f3
jeffte costa <jefftewf17@outlook.com>
2023-01-31 16:14:37 -0300

    feat: add navigation to SignUp screen

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,8 @@
+import { useNavigation } from "@react-navigation/native";
+
 import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
+
+import { AuthNavigatorRoutesProps } from "@routes/auth.routes";
 
 import LogoSvg from "@assets/logo.svg";
 import BackgroundImg from "@assets/background.png";
@@ -6,6 +10,12 @@ import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 
 export function SignIn() {
+	const navigation = useNavigation<AuthNavigatorRoutesProps>();
+
+	function handleNewAccount() {
+		navigation.navigate("signUp");
+	}
+
 	return (
 		<ScrollView
 			contentContainerStyle={{ flexGrow: 1 }}
@@ -40,7 +50,11 @@ export function SignIn() {
 					<Text color="gray.100" fontSize="sm" mb="3" fontFamily="body">
 						Ainda não tem acesso?
 					</Text>
-					<Button title="Criar conta" variant="outline" />
+					<Button
+						title="Criar conta"
+						variant="outline"
+						onPress={handleNewAccount}
+					/>
 				</Center>
 			</VStack>
 		</ScrollView>
