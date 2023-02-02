7e62f55d5ea2309e41a474e06a2a24bae666b5d4
jeffte costa <jefftewf17@outlook.com>
2023-02-02 16:38:47 -0300

    feat: show active group

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,12 +1,26 @@
 import { Group } from "@components/Group";
 import { HomeHeader } from "@components/HomeHeader";
-import { Center, VStack } from "native-base";
+import { HStack, VStack } from "native-base";
+import { useState } from "react";
 
 export function Home() {
+	const [groupSelected, setGroupSelected] = useState("costa");
+
 	return (
 		<VStack>
 			<HomeHeader />
-			<Group name="costas" />
+			<HStack>
+				<Group
+					name="costa"
+					isActive={groupSelected === "costa"}
+					onPress={() => setGroupSelected("costa")}
+				/>
+				<Group
+					name="ombro"
+					isActive={groupSelected === "ombro"}
+					onPress={() => setGroupSelected("ombro")}
+				/>
+			</HStack>
 		</VStack>
 	);
 }
