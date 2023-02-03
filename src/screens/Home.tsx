50a5f098c15f46f09329de1c1b3325b6d0cbbf89
jeffte costa <jefftewf17@outlook.com>
2023-02-03 11:00:10 -0300

    feat: add Header of ExerciseCard list

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 import { Group } from "@components/Group";
 import { HomeHeader } from "@components/HomeHeader";
-import { VStack, FlatList } from "native-base";
+import { VStack, FlatList, HStack, Heading, Text } from "native-base";
 
 export function Home() {
 	const [groups, setGroups] = useState([
@@ -33,6 +33,17 @@ export function Home() {
 				my={10}
 				maxH={10}
 			/>
+
+			<VStack px={8}>
+				<HStack justifyContent="space-between" mb={5}>
+					<Heading color="gray.200" fontSize="md">
+						Exercicios
+					</Heading>
+					<Text color="gray.200" fontSize="sm">
+						4
+					</Text>
+				</HStack>
+			</VStack>
 		</VStack>
 	);
 }
