bacf44ec7fe94879e9aac3989eb2ddd51b6bcef0
jeffte costa <jefftewf17@outlook.com>
2023-02-03 10:50:27 -0300

    feat: add FlatList to list all groups

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,26 +1,38 @@
-import { Group } from "@components/Group";
-import { HomeHeader } from "@components/HomeHeader";
-import { HStack, VStack } from "native-base";
 import { useState } from "react";
 
+import { Group } from "@components/Group";
+import { HomeHeader } from "@components/HomeHeader";
+import { VStack, FlatList } from "native-base";
+
 export function Home() {
+	const [groups, setGroups] = useState([
+		"costas",
+		"biceps",
+		"triceps",
+		"ombro",
+	]);
 	const [groupSelected, setGroupSelected] = useState("costa");
 
 	return (
 		<VStack>
 			<HomeHeader />
-			<HStack>
-				<Group
-					name="costa"
-					isActive={groupSelected === "costa"}
-					onPress={() => setGroupSelected("costa")}
-				/>
-				<Group
-					name="ombro"
-					isActive={groupSelected === "ombro"}
-					onPress={() => setGroupSelected("ombro")}
-				/>
-			</HStack>
+
+			<FlatList
+				data={groups}
+				keyExtractor={(item) => item}
+				renderItem={({ item }) => (
+					<Group
+						name={item}
+						isActive={groupSelected === item}
+						onPress={() => setGroupSelected(item)}
+					/>
+				)}
+				horizontal
+				showsHorizontalScrollIndicator={false}
+				_contentContainerStyle={{ px: 8 }}
+				my={10}
+				maxH={10}
+			/>
 		</VStack>
 	);
 }
