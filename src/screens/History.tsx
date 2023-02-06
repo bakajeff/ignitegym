cb37fc0e1b4920c6a75b17607a819b52e93a7539
jeffte costa <jefftewf17@outlook.com>
2023-02-06 15:34:31 -0300

    feat: add ScreenHeader component

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,9 +1,10 @@
-import { Center, Text } from "native-base";
+import { ScreenHeader } from "@components/ScreenHeader";
+import { Text, VStack } from "native-base";
 
 export function History() {
 	return (
-		<Center>
-			<Text>History</Text>
-		</Center>
+		<VStack>
+			<ScreenHeader title="histórico de exercícios" />
+		</VStack>
 	);
 }
