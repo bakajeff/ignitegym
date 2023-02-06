f37b9f68649fa15a27cd73fef9ba7784ad706cf7
jeffte costa <jefftewf17@outlook.com>
2023-02-06 16:11:30 -0300

    feat: add list empty fallback

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Heading, VStack, SectionList } from "native-base";
+import { Heading, VStack, SectionList, Text } from "native-base";
 
 import { HistoryCard } from "@components/HistoryCard";
 import { ScreenHeader } from "@components/ScreenHeader";
@@ -7,16 +7,16 @@ import { ScreenHeader } from "@components/ScreenHeader";
 export function History() {
 	const [exercises, setExercises] = useState([
 		{
-			tittle: "26.08.22",
+			title: "26.08.22",
 			data: ["Puxada frontal", "Remada unilateral"],
 		},
 		{
-			tittle: "27.08.22",
+			title: "27.08.22",
 			data: ["Puxada frontal"],
 		},
 	]);
 	return (
-		<VStack>
+		<VStack flex={1}>
 			<ScreenHeader title="histórico de exercícios" />
 
 			<SectionList
@@ -25,10 +25,19 @@ export function History() {
 				renderItem={({ item }) => <HistoryCard />}
 				renderSectionHeader={({ section }) => (
 					<Heading color="gray.200" fontSize="md" mt="10" mb={3}>
-						{section.tittle}
+						{section.title}
 					</Heading>
 				)}
 				px={8}
+				contentContainerStyle={
+					exercises.length === 0 && { flex: 1, justifyContent: "center" }
+				}
+				ListEmptyComponent={() => (
+					<Text color="gray.100" textAlign="center">
+						Não há exercícios registrados ainda. {"\n"} Vamos fazer exercício
+						hoje?
+					</Text>
+				)}
 			/>
 		</VStack>
 	);
