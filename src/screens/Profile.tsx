810182172d5dbd43c524a8c9101f97eb3b5c6add
jeffte costa <jefftewf17@outlook.com>
2023-02-07 16:33:31 -0300

    feat: add button to change profile photo

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,19 +1,46 @@
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
-import { Center, ScrollView, VStack } from "native-base";
+import { Center, ScrollView, VStack, Skeleton, Text } from "native-base";
+import { useState } from "react";
+import { TouchableOpacity } from "react-native";
+
+const PHOTO_SIZE = 33;
 
 export function Profile() {
+	const [photoIsLoading, setPhotoIsLoading] = useState(false);
+
 	return (
 		<VStack flex={1}>
 			<ScreenHeader title="Perfil" />
 
 			<ScrollView>
 				<Center mt={6} px={10}>
-					<UserPhoto
-						source={{ uri: "https://github.com/bakajeff.png" }}
-						size={33}
-						alt=""
-					/>
+					{photoIsLoading ? (
+						<Skeleton
+							w={PHOTO_SIZE}
+							h={PHOTO_SIZE}
+							rounded="full"
+							startColor="gray.500"
+							endColor="gray.400"
+						/>
+					) : (
+						<UserPhoto
+							source={{ uri: "https://github.com/bakajeff.png" }}
+							size={PHOTO_SIZE}
+							alt=""
+						/>
+					)}
+					<TouchableOpacity>
+						<Text
+							color="green.500"
+							fontWeight="bold"
+							fontSize="md"
+							mt={2}
+							mb={8}
+						>
+							Alterar foto
+						</Text>
+					</TouchableOpacity>
 				</Center>
 			</ScrollView>
 		</VStack>
