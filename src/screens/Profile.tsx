6d94666a8ff2c83eeb9d3d00fb44ba28656968e6
jeffte costa <jefftewf17@outlook.com>
2023-02-09 16:46:01 -0300

    feat: add skeleton while loading

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -22,18 +22,26 @@ export function Profile() {
 	const [userPhoto, setUserPhoto] = useState("https://github.com/bakajeff.png");
 
 	async function handleUserPhotoSelect() {
-		const photoSelected = await ImagePicker.launchImageLibraryAsync({
-			mediaTypes: ImagePicker.MediaTypeOptions.Images,
-			quality: 1,
-			aspect: [4, 4],
-			allowsEditing: true,
-		});
+		setPhotoIsLoading(true);
 
-		if (photoSelected.canceled) {
-			return;
+		try {
+			const photoSelected = await ImagePicker.launchImageLibraryAsync({
+				mediaTypes: ImagePicker.MediaTypeOptions.Images,
+				quality: 1,
+				aspect: [4, 4],
+				allowsEditing: true,
+			});
+
+			if (photoSelected.canceled) {
+				return;
+			}
+
+			setUserPhoto(photoSelected.assets[0].uri);
+		} catch (error) {
+			console.log(error);
+		} finally {
+			setPhotoIsLoading(false);
 		}
-
-		setUserPhoto(photoSelected.assets[0].uri);
 	}
 
 	return (
