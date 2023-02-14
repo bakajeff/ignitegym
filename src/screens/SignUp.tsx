24251e6c3e6d2004123607aecf282baea1672c07
jeffte costa <jefftewf17@outlook.com>
2023-02-14 15:35:49 -0300

    refactor: add FormControl at Input component

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -62,12 +62,14 @@ export function SignUp() {
 						}}
 						name="name"
 						render={({ field: { onChange, value } }) => (
-							<Input placeholder="Nome" onChangeText={onChange} value={value} />
+							<Input
+								placeholder="Nome"
+								onChangeText={onChange}
+								value={value}
+								errorMessage={errors.name?.message}
+							/>
 						)}
 					/>
-
-					<Text color="white">{errors.name?.message}</Text>
-
 					<Controller
 						control={control}
 						name="email"
@@ -85,11 +87,10 @@ export function SignUp() {
 								autoCapitalize="none"
 								onChangeText={onChange}
 								value={value}
+								errorMessage={errors.email?.message}
 							/>
 						)}
 					/>
-
-					<Text color="white">{errors.email?.message}</Text>
 
 					<Controller
 						control={control}
