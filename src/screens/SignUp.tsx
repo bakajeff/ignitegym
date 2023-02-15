1e98dbcb0df56d109dc9342e8debbbe418baa748
jeffte costa <jefftewf17@outlook.com>
2023-02-15 16:40:38 -0300

    feat: catch backend response

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -52,7 +52,9 @@ export function SignUp() {
 				"Content-Type": "application/json",
 			},
 			body: JSON.stringify({ name, email, password }),
-		});
+		})
+			.then((response) => response.json())
+			.then((data) => console.log(data));
 	}
 
 	return (
