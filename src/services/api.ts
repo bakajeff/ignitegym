173ab5e554f35d7d68750bd8b96418e133118ebf
jeffte costa <jefftewf17@outlook.com>
2023-02-16 11:12:35 -0300

    feat: add axios interceptor

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,23 @@
 import axios from "axios";
 import Constants from "expo-constants";
 
+import { AppError } from "@utils/AppError";
+
 const apiUrl = Constants.expoConfig?.extra?.apiUrl;
 
-console.log(apiUrl);
-
-export const api = axios.create({
+const api = axios.create({
 	baseURL: apiUrl,
 });
+
+api.interceptors.response.use(
+	(response) => response,
+	(error) => {
+		if (error.response?.data) {
+			return Promise.reject(new AppError(error.response.data.message));
+		} else {
+			return Promise.reject(error);
+		}
+	},
+);
+
+export { api };
