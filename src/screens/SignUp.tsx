be6e48d679f7b1cce77fdcb647880165a56b193e
jeffte costa <jefftewf17@outlook.com>
2023-03-01 16:48:33 -0300

    feat: redirect user on signUp

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
 	VStack,
 	Image,
@@ -21,6 +22,7 @@ import { Button } from "@components/Button";
 import { useNavigation } from "@react-navigation/native";
 
 import { AppError } from "@utils/AppError";
+import { useAuth } from "@hooks/useAuth";
 
 type FormDataProps = {
 	name: string;
@@ -43,9 +45,13 @@ const signUpSchema = yup.object({
 });
 
 export function SignUp() {
+	const [isLoading, setIsLoading] = useState(false);
+
 	const navigation = useNavigation();
 
 	const toast = useToast();
+
+	const { signIn } = useAuth();
 
 	const {
 		control,
@@ -61,8 +67,14 @@ export function SignUp() {
 
 	async function handleSignUp({ name, email, password }: FormDataProps) {
 		try {
-			const response = await api.post("/users", { name, email, password });
+			setIsLoading(true);
+
+			await api.post("/users", { name, email, password });
+
+			await signIn(email, password);
 		} catch (error) {
+			setIsLoading(false);
+
 			const isAppError = error instanceof AppError;
 			const title = isAppError
 				? error.message
@@ -163,6 +175,7 @@ export function SignUp() {
 					<Button
 						title="Criar e acessar"
 						onPress={handleSubmit(handleSignUp)}
+						isLoading={isLoading}
 					/>
 				</Center>
 
