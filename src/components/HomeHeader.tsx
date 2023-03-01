20115f6c8e1a45a16cccfc9bec90996f02a7ca6e
jeffte costa <jefftewf17@outlook.com>
2023-03-01 16:38:08 -0300

    feat: add default user photo

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,10 +1,12 @@
-import { Heading, HStack, Text, VStack, Icon } from "native-base";
-import { MaterialIcons } from "@expo/vector-icons";
-
-import { UserPhoto } from "./UserPhoto";
 import { TouchableOpacity } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Heading, HStack, Text, VStack, Icon } from "native-base";
 
 import { useAuth } from "@hooks/useAuth";
+
+import { UserPhoto } from "./UserPhoto";
+
+import defaultUserPhotoImg from "@assets/userPhotoDefault.png";
 
 export function HomeHeader() {
 	const { user } = useAuth();
@@ -18,15 +20,21 @@ export function HomeHeader() {
 			alignItems="center"
 		>
 			<UserPhoto
-				source={{ uri: "https://github.com/bakajeff.png" }}
+				source={
+					user.avatar
+						? { uri: "https://github.com/bakajeff.png" }
+						: defaultUserPhotoImg
+				}
 				size={16}
 				alt=""
 				mr={4}
 			/>
+
 			<VStack flex={1}>
 				<Text color="gray.100" fontSize="md">
 					Olá,
 				</Text>
+
 				<Heading color="gray.100" fontSize="md" fontFamily="heading">
 					{user.name}
 				</Heading>
