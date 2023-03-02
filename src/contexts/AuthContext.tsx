1b342fae9d2d5bfd0c2e8aa31abb938143a0bfa7
jeffte costa <jefftewf17@outlook.com>
2023-03-02 15:45:12 -0300

    feat: add token on api header

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -7,6 +7,8 @@ import {
 	storageUserSave,
 } from "@storage/storageUser";
 
+import { storageAuthTokenSave } from "@storage/storageAuthToken";
+
 import { api } from "@services/api";
 
 export type AuthContextDataProps = {
@@ -29,13 +31,29 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
 	const [isLoadingUserStorageData, setIsLoadingUserStorageData] =
 		useState(true);
 
+	async function storageUserAndToken(userData: UserDTO, token: string) {
+		try {
+			setIsLoadingUserStorageData(true);
+
+			api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+
+			await storageUserSave(userData);
+			await storageAuthTokenSave(token);
+
+			setUser(user);
+		} catch (error) {
+			throw error;
+		} finally {
+			setIsLoadingUserStorageData(false);
+		}
+	}
+
 	async function signIn(email: string, password: string) {
 		try {
 			const { data } = await api.post("/sessions", { email, password });
 
 			if (data.user && data.token) {
-				setUser(data.user);
-				storageUserSave(data.user);
+				storageUserAndToken(data.user, data.token);
 			}
 		} catch (error) {
 			throw error;
