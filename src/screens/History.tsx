75c484c5af6480d1fabc927ef8026ceca5f5b85e
jeffte costa <jefftewf17@outlook.com>
2023-03-08 16:11:59 -0300

    feat: fetch History

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,10 +1,15 @@
-import { useState } from "react";
-import { Heading, VStack, SectionList, Text } from "native-base";
+import { useCallback, useState } from "react";
+import { Heading, VStack, SectionList, Text, useToast } from "native-base";
 
 import { HistoryCard } from "@components/HistoryCard";
 import { ScreenHeader } from "@components/ScreenHeader";
 
+import { AppError } from "@utils/AppError";
+import { api } from "@services/api";
+import { useFocusEffect } from "@react-navigation/native";
+
 export function History() {
+	const [isLoading, setIsLoading] = useState(true);
 	const [exercises, setExercises] = useState([
 		{
 			title: "26.08.22",
@@ -15,6 +20,36 @@ export function History() {
 			data: ["Puxada frontal"],
 		},
 	]);
+
+	const toast = useToast();
+
+	async function fetchHistory() {
+		try {
+			setIsLoading(true);
+			const response = await api.get("/history");
+			console.log(response.data);
+		} catch (error) {
+			const isAppError = error instanceof AppError;
+			const title = isAppError
+				? error.message
+				: "Não foi possível registrar o histórico.";
+
+			toast.show({
+				title,
+				placement: "top",
+				bgColor: "red.500",
+			});
+		} finally {
+			setIsLoading(false);
+		}
+	}
+
+	useFocusEffect(
+		useCallback(() => {
+			fetchHistory();
+		}, []),
+	);
+
 	return (
 		<VStack flex={1}>
 			<ScreenHeader title="histórico de exercícios" />
